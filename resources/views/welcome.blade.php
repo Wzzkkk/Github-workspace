984144068de56abda642bb82ba189244e590bf31
Wzzkkk <1061038365@qq.com>
2025-11-25 21:57:46 +0800

    美化主页按钮样式

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,26 +11,26 @@
 <body>
 
     <div class="profile-card">
-        <img src="/avatar.png" alt="头像" class="avatar">
+        <img src="/avatar.jpg" alt="头像" class="avatar">
         
         <h1>Wzzkkk</h1>
         <p>全栈开发者 / 超级无敌ZZZ</p>
         <p>这是魏的个人网站，运行在 Ubuntu VPS 上。</p>
         
-       <div class="social-links">
-            <a href="https://github.com/Wzzkkk" target="_blank" class="btn btn-github">
-            <i class="fab fa-github"></i> GitHub
-            </a>
-            <a href="mailto:admin@zwk369.me" class="btn btn-email">
-            <i class="fas fa-envelope"></i> Email
-            </a>
-            <a href="/about" class="btn btn-about">
-            <i class="fas fa-user"></i> 关于我
-            </a>
-            <a href="/guestbook" class="btn btn-guestbook">
-            <i class="fas fa-comment-alt"></i> 留言板
-            </a>
-        </div>
+      <div class="social-links">
+    <a href="https://github.com/Wzzkkk" target="_blank" class="btn btn-github">
+        <i class="fab fa-github"></i> GitHub
+    </a>
+    <a href="mailto:admin@zwk369.me" class="btn btn-email">
+        <i class="fas fa-envelope"></i> Email
+    </a>
+    <a href="/about" class="btn btn-about">
+        <i class="fas fa-user"></i> 关于我
+    </a>
+    <a href="/guestbook" class="btn btn-guestbook">
+        <i class="fas fa-comment-alt"></i> 留言板
+    </a>
+</div>
 
     </div>
 
