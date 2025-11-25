6a2711975d026f8f7e0fa7e4c0cf00398ce85735
Wzzkkk <1061038365@qq.com>
2025-11-25 21:33:17 +0800

    Change profile avatar to local image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,23 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Wzzkkk 的个人主页</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="/css/style.css">
-    <link rel="icon" type="image/jpg" href="/favicon.jpg">// 网站图标
+    <link rel="icon" type="image/jpg" href="/favicon.jpg">
 </head>
 <body>
 
     <div class="profile-card">
-        <img src="https://api.dicebear.com/7.x/avataaars/svg?seed=Felix" alt="头像" class="avatar">
+        <img src="/avatar.png" alt="头像" class="avatar">
         
         <h1>Wzzkkk</h1>
         <p>全栈开发者 / 超级无敌ZZZ</p>
         <p>这是魏的个人网站，运行在 Ubuntu VPS 上。</p>
         
-        <div class="social-links">
-            <a href="https://github.com/Wzzkkk" target="_blank" class="btn btn-github">GitHub</a>
-            <a href="mailto:admin@zwk369.me" class="btn btn-email">Email</a>
-            <a href="/about" class="btn" style="background-color: #28a745;">关于我</a>
-            <a href="/guestbook" class="btn" style="background-color: #ff9f43;">留言板</a>
+       <div class="social-links">
+            <a href="https://github.com/Wzzkkk" target="_blank" class="btn btn-github">
+            <i class="fab fa-github"></i> GitHub
+            </a>
+            <a href="mailto:admin@zwk369.me" class="btn btn-email">
+            <i class="fas fa-envelope"></i> Email
+            </a>
+            <a href="/about" class="btn btn-about">
+            <i class="fas fa-user"></i> 关于我
+            </a>
+            <a href="/guestbook" class="btn btn-guestbook">
+            <i class="fas fa-comment-alt"></i> 留言板
+            </a>
         </div>
 
     </div>
